3c1907f4fb5f3a06f9bffd536b44453678196cd8
alunoif <aluno>
2025-10-01 17:24:04 -0300

    aula 01-10

--- src/views/entregador/ListEntregador.jsx
+++ src/views/entregador/ListEntregador.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Container, Divider, Header, Icon, Modal, Table } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
-export default function ListEntregador() {
 
+
+export default function ListEntregador() {
+const [openModal, setOpenModal] = useState(false);
+const [idRemover, setIdRemover] = useState();
     const [lista, setLista] = useState([]);
 
     useEffect(() => {
@@ -29,9 +32,35 @@ export default function ListEntregador() {
         return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
     }
 
+    function confirmaRemover(id) {
+        setOpenModal(true)
+        setIdRemover(id)
+    }
+
+
+    async function remover() {
+
+        await axios.delete('http://localhost:8080/api/entregador/' + idRemover)
+            .then((response) => {
+
+                console.log('Entregador removido com sucesso.')
+
+                axios.get("http://localhost:8080/api/entregador")
+                    .then((response) => {
+                        setLista(response.data)
+                    })
+            })
+            .catch((error) => {
+                console.log('Erro ao remover um entregador.')
+            })
+        setOpenModal(false)
+    }
+
+
+
     return (
         <div>
-            <MenuSistema tela={'cliente'} />
+            <MenuSistema tela={'entregador'} />
             <div style={{ marginTop: '3%' }}>
 
                 <Container textAlign='justified' >
@@ -108,7 +137,7 @@ export default function ListEntregador() {
                                                 color='green'
                                                 title='Clique aqui para editar os dados deste entregador'
                                                 icon>
-                                                 <Link to="/form-entregador" state={{ id: entregador.id }} style={{ color: 'green' }}> <Icon name='edit' /> </Link>
+                                                <Link to="/form-entregador" state={{ id: entregador.id }} style={{ color: 'green' }}> <Icon name='edit' /> </Link>
                                             </Button> &nbsp;
 
                                             <Button
@@ -116,7 +145,8 @@ export default function ListEntregador() {
                                                 circular
                                                 color='red'
                                                 title='Clique aqui para remover este entregador'
-                                                icon>
+                                                icon
+                                                onClick={e => confirmaRemover(entregador.id)}>
                                                 <Icon name='trash' />
                                             </Button>
 
@@ -129,6 +159,25 @@ export default function ListEntregador() {
                     </div>
                 </Container>
             </div>
+            <Modal
+                basic
+                onClose={() => setOpenModal(false)}
+                onOpen={() => setOpenModal(true)}
+                open={openModal}
+            >
+                <Header icon>
+                    <Icon name='trash' />
+                    <div style={{ marginTop: '5%' }}> Tem certeza que deseja remover esse registro? </div>
+                </Header>
+                <Modal.Actions>
+                    <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                        <Icon name='remove' /> Não
+                    </Button>
+                    <Button color='green' inverted onClick={() => remover()}>
+                        <Icon name='checkmark' /> Sim
+                    </Button>
+                </Modal.Actions>
+            </Modal>
 
         </div>
     )
